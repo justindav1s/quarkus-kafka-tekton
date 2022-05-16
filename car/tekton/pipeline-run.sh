fe6f0d55d061e309e5801f5fa0b8407705aed5ed
Justin Davis <justinndavis@gmail.com>
2022-05-16 14:32:36 +0100

    fix

--- car/tekton/pipeline-run.sh
+++ car/tekton/pipeline-run.sh
@@ -5,18 +5,18 @@ PROJECT=connected
 APP=car
 PROFILE=dev
 
-oc delete configmap ${APP}-${PROFILE}-config ${APP}-${PROFILE}-user-truststore
+oc delete configmap ${APP}-${PROFILE}-config ${APP}-${PROFILE}-kafka-truststore
 
 oc create configmap ${APP}-${PROFILE}-config \
     --from-file=../src/main/resources/config.${PROFILE}.properties \
     -n ${PROJECT}
 
-oc create configmap ${APP}-${PROFILE}-user-truststore \
-    --from-file=../truststore/user-truststore.jks \
+oc create configmap ${APP}-${PROFILE}-kafka-truststore \
+    --from-file=../truststore/kafka-truststore.jks \
     -n ${PROJECT}
 
 oc label configmap ${APP}-${PROFILE}-config app=${APP}
-oc label configmap ${APP}-${PROFILE}-user-truststore app=${APP}
+oc label configmap ${APP}-${PROFILE}-kafka-truststore app=${APP}
 
 cat << EOF > ${APP}-${PROFILE}-pipeline-pvc.yaml
 apiVersion: v1
@@ -36,12 +36,12 @@ EOF
 tkn pipeline start maven-build-test-deploy \
     -w name=shared-workspace,volumeClaimTemplateFile=${APP}-${PROFILE}-pipeline-pvc.yaml \
     -w name=maven-settings,config=custom-maven-settings \
-    -w name=truststore,config=${APP}-${PROFILE}-user-truststore \
+    -w name=truststore,config=${APP}-${PROFILE}-kafka-truststore \
     -p APP_NAME=${APP} \
-    -p GIT_REPO=https://github.com/justindav1s/microservices-on-openshift.git \
-    -p GIT_BRANCH=master \
+    -p GIT_REPO=https://github.com/justindav1s/quarkus-kafka-tekton.git \
+    -p GIT_BRANCH=main \
     -p APP_PROFILE=${PROFILE} \
-    -p CONTEXT_DIR=src/quarkus/${APP} \
+    -p CONTEXT_DIR=${APP} \
     -p IMAGE_REPO=${QUAYIO_HOST}/${QUAYIO_USER} \
     --use-param-defaults \
     --showlog
