52dca18a1f446dda8cd694b93b20f7494c9dd0b5
Justin Davis <justinndavis@gmail.com>
2022-05-17 12:07:40 +0100

    fix

--- cars/native-build/build-deploy.sh
+++ cars/native-build/build-deploy.sh
@@ -5,7 +5,7 @@ REGISTRY_HOST=quay.io
 REPO=justindav1s
 VERSION=latest
 
-docker build -t $IMAGE - < Dockerfile
+docker build -t $IMAGE - < Dockerfile.graal2
 
 TAG=$REGISTRY_HOST/$REPO/$IMAGE:$VERSION
 
