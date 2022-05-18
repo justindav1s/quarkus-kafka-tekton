6ee4394544e6e9699adf1988c2b54fd1545ef88c
Justin Davis <justinndavis@gmail.com>
2022-05-18 10:47:36 +0100

    updates

--- apps/cars/truststore/setup.sh
+++ apps/cars/truststore/setup.sh
@@ -5,6 +5,6 @@ rm -rf kafka-truststore.jks
 oc get secret my-cluster2-cluster-ca-cert -n kafka-cluster2 -o jsonpath='{.data.ca\.crt}' | base64 --decode > ca.crt
 
 PASSWORD=monkey123
-keytool -import -trustcacerts -file ca.crt -keystore kafka-truststore.jks -storepass $PASSWORD -noprompt
+keytool -import -trustcacerts -file ca.crt -keystore kafka-dev-truststore.jks -storepass $PASSWORD -noprompt
 
 rm -rf ca.crt
